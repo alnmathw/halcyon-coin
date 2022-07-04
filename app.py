c2245635d253eba39931aaf4cc74d309b368b233
alan-matthew <alan.matthew.anggara@gmail.com>
2022-07-04 17:34:08 +0800

    implement logout

--- app.py
+++ app.py
@@ -39,39 +39,53 @@ def register():
 
     return render_template('register.html', form=form)
 
-#Login page
-@app.route("/login", methods = ['GET', 'POST'])
+# Login page
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-     #if form is submitted
+    # if form is submitted
     if request.method == 'POST':
-        #collect form data
+        # collect form data
         username = request.form['username']
         candidate = request.form['password']
 
-        #access users table to get the user's actual password
+        # access users table to get the user's actual password
         users = Table("users", "name", "email", "username", "password")
         user = users.getone("username", username)
         accPass = user.get('password')
 
-        #if the password cannot be found, the user does not exist
+        # if the password cannot be found, the user does not exist
         if accPass is None:
             flash("Username is not found", 'danger')
             return redirect(url_for('login'))
         else:
-            #verify that the password entered matches the actual password
+            # verify that the password entered matches the actual password
             if sha256_crypt.verify(candidate, accPass):
-                #log in the user and redirect to Dashboard page
+                # log in the user and redirect to Dashboard page
                 log_in_user(username)
                 flash('You are now logged in.', 'success')
                 return redirect(url_for('dashboard'))
             else:
-                #if the passwords do not match
+                # if the passwords do not match
                 flash("Invalid password", 'danger')
                 return redirect(url_for('login'))
 
     return render_template('login.html')
 
-#Dashboard page
+# logout the user. Ends current session
+
+
+@app.route("/logout")
+@is_logged_in
+def logout():
+    session.clear()
+    flash("Logout success", "success")
+    return redirect(url_for('login'))
+
+# Dashboard page
+
+
 @app.route("/dashboard")
 @is_logged_in
 def dashboard():
