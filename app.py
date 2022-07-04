bdaa23bc95b397eb57240568477f8dcecc44358e
alan-matthew <alan.matthew.anggara@gmail.com>
2022-07-04 17:23:21 +0800

    improved registeration forms and validation for 1) a form submission and 2) if a user is a new user

--- app.py
+++ app.py
@@ -19,8 +19,24 @@ mysql = MySQL(app)
 def register():
     form = RegisterForm(request.form)
     users = Table("users", "name", "email", "username", "password")
+
+    # if form is submitted
     if request.method == 'POST' and form.validate():
-        pass
+        # collect form data
+        username = form.username.data
+        email = form.email.data
+        name = form.name.data
+        # make sure user does not already exist
+        if isnewuser(username):
+            # add the user to mysql and log them in
+            password = sha256_crypt.encrypt(form.password.data)
+            users.insert(name, email, username, password)
+            log_in_user(username)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('User already exists', 'danger')
+            return redirect(url_for('register'))
+
     return render_template('register.html', form=form)
 
 
