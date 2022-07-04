7400e16231e425edd0f7ab9a227ef00c8a952630
alan-matthew <alan.matthew.anggara@gmail.com>
2022-07-04 18:04:39 +0800

    setup buy routing

--- app.py
+++ app.py
@@ -99,6 +99,49 @@ def login():
 
     return render_template('login.html')
 
+# Transaction page
+
+
+@app.route("/transaction", methods=['GET', 'POST'])
+@is_logged_in
+def transaction():
+    form = SendMoneyForm(request.form)
+    balance = get_balance(session.get('username'))
+
+    # if form is submitted
+    if request.method == 'POST':
+        try:
+            # attempt to execute the transaction
+            send_money(session.get('username'),
+                       form.username.data, form.amount.data)
+            flash("Money Sent!", "success")
+        except Exception as e:
+            flash(str(e), 'danger')
+
+        return redirect(url_for('transaction'))
+
+    return render_template('transaction.html', balance=balance, form=form, page='transaction')
+
+# Buy page
+
+
+@app.route("/buy", methods=['GET', 'POST'])
+@is_logged_in
+def buy():
+    form = BuyForm(request.form)
+    balance = get_balance(session.get('username'))
+
+    if request.method == 'POST':
+        # attempt to buy amount
+        try:
+            send_money("BANK", session.get('username'), form.amount.data)
+            flash("Purchase Successful!", "success")
+        except Exception as e:
+            flash(str(e), 'danger')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('buy.html', balance=balance, form=form, page='buy')
 # logout the user. Ends current session
 
 
